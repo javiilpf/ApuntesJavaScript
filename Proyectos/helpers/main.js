9a230311d8914af592636bddacf3047a0ae415d6
javiilpf <javieraviless2003@gmail.com>
2024-09-24 18:57:23 +0200

    Deberes javascript

--- Proyectos/helpers/main.js
+++ Proyectos/helpers/main.js
@@ -1,14 +1,17 @@
-import {saludar, saludar2, esPrimo, factorial, funcion2grado} from './script.js'
-console.log(saludar);
+import {validacionBloques, saludar, saludar2, esPrimo, factorial, funcion2grado, buscarTransaccion} from './script.js'
+//console.log(saludar);
 
 document.addEventListener("DOMContentLoaded",() => {
-    console.log(esPrimo(7));
+    //console.log(esPrimo(7));
 
+    
 
+    //console.log (funcion2grado(1,-5,6));
 
-    console.log (funcion2grado(1,-5,6));
+    //console.log(factorial(5));
 
-    console.log(factorial(5));
+    //console.log(buscarTransaccion('0xmnq'));
+    console.log(validacionBloques() );
 });
 
 
--- Proyectos/helpers/script.js
+++ Proyectos/helpers/script.js
@@ -131,25 +131,82 @@ export const lastImpar =numeros => numeros.filter((numero) => numero%2 !==0).pop
 export const buscarCiudad = (citysArray, oldCity, newCity) => {
   citys.map(city=>city=== oldCity ? newCity:city);
 }
-
+const transacciones =[
+	{id:1, mont:8, dirección:'0xabc'},
+	{id:2, mont:15, dirección:'0xdfe'},
+	{id:3, mont:22, dirección:'0xjkl'},
+	{id:4, mont:8, dirección:'0xmnq'}
+]
 /**
  * @description: Funcion que filtra transacciones cuyo mont sea mayor de 12 ordenadas de mayor a menor
  * @param {Object} transacciones
  * @returns {Object}
  * @version 1.0
  */
-export const montMayorMenor =(transacciones)=>{
+export const montMayorMenor=()=>{
   return transacciones.map(transaccion=>transaccion.mount>=12).sort((a,b)=>a-b).reverse();
 
+}
+
+/**
+ * @description: Función que busca la dirección de la transacción y cuando la encuentre diga cuantas transacciones hay mayores y menores que esa.
+ * @param {Object} transacciones
+ * @returns {String}
+ * @version 1.0
+ */
+export const buscarTransaccion = (direccion) => {
+  const transaccionNombrada = transacciones.find(transaccion => transaccion.direccion === direccion);
+
+  if (!transaccionNombrada) {
+    return `No se encontró una transacción con la dirección: ${direccion}`;
+  }
+
+  let transaccionesMenores= 0;
+  let transaccionesMayores = 0;
+  transacciones.map(transaccion => {
+    if (transaccion.mont !== undefined) {
+      if (transaccion.mont < transaccionNombrada.mont) {
+        transaccionesMenores++;
+      } else if (transaccion.mont > transaccionNombrada.mont) {
+        transaccionesMayores++;
+      }
+    }
+    return transaccion; 
+  });
+
+  return `La dirección: ${direccion} tiene ${transaccionesMenores} transacciones por debajo y ${transaccionesMayores} transacciones por encima.`;
+};
+
+const bloques = [
+	{ id : 1, hash: "abc123", prevHas: null	},
+	{ id : 2, hash: "def456", prevHas:'abc123'},
+	{ id : 3, hash: "ghi789", prevHas:'def456'},
+	{ id : 4, hash: "jkl012", prevHas:'ghi789'},
+]
+/**
+ * @description: Función que valida los bloques 
+ * @param {number} numero
+ * @returns {number}
+ * @version 1.0
+ * */
+export const validacionBloques =()=>{
+  let respuesta =[];
+  for(let i=1; i<bloques.length; i++){
+    if (bloques[i].prevHas!==bloques[i-1].hash){
+      respuesta.push( 'No se mantiene la integridad');
+    } else {
+      respuesta.push('Se mantiene la integridad');
+    }
+  }
   
-    
+  return respuesta;
 }
 //----------Inicio de la aplicación-----------
 
-console.log(saludar2("Pepe"));
+// console.log(saludar2("Pepe"));
 
-console.log(numero('2'));
+// console.log(numero('2'));
 
-console.log (numero2("Manuel"));
+// console.log (numero2("Manuel"));
 
-console.log(esPrimo(6));
+// console.log(esPrimo(6));
